4acee1f4fc21c28d83c7e0a6c3d540889300eb1a
ipekbensu <ibmanav@gmail.com>
2019-03-01 17:06:45 -0500

    added getData.js

---
+++ getData.js
@@ -0,0 +1,22 @@
+// QUESTION 2
+// FETCH DATA
+
+var url = 'https://api-v3.mbta.com/vehicles?api_key=b931eb2686024e9a8efca565d7c894e1&filter[route]=1&include=trip';
+var count = 0;
+function getData(){
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            data = json.data;
+            console.log(data);
+            if(count == 0){
+                mapInit();
+                markersInit();
+                count = 1;
+            }
+            if(count == 1){
+                markersMove();
+            }
+            setTimeout(getData, 15000);
+        })
+};
